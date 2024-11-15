a689b54f1024ebdd2c0397b51bb335271c14a636
Hariprasad Sakhare <hariprasadsakhare@MacBook-Air.local>
2024-11-15 10:43:06 +0530

    this is my first commet

---
+++ Chapter1/module.py
@@ -0,0 +1,14 @@
+import pyjokes
+
+# print("printing jokes.....")
+joke =pyjokes.get_joke()
+print(joke)
+""""
+thanks that was my gandiv dadu
+gapu 
+pappu
+golu 
+"""
+# dadu you can be great a
+# t doing 
+# coding 
