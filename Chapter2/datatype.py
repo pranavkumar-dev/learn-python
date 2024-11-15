b2f67ffebd5dc8067a80596ff9eec0be6b7e2121
pranavkumar sakhare <pranavkumar6328@gmail.com>
2024-11-15 10:58:08 +0530

    new code update

---
+++ Chapter2/datatype.py
@@ -0,0 +1,9 @@
+a = 5 # a is a integer
+
+b = 63.28 # b is a floating integers
+
+c = "harry" # c is a string
+
+d= False # d is a boolean variable
+
+e = None # e is a none type variable
---
+++ Chapter2/operators.py
@@ -0,0 +1,5 @@
+# arthematic operators 
+a = 5
+b = 6
+c = a+b
+print(c)
